8c4ae00692444f59ff8de139b4fb239e97cf89e8
Raymand <yongconglei@gmail.com>
2018-03-10 16:29:58 -0500

    [Topic] Implement asynchronous controller [Todo] Write java doc and test case

--- app/controllers/WidgetController.java
+++ app/controllers/WidgetController.java
@@ -10,24 +10,22 @@ import javax.inject.Inject;
 import javax.inject.Singleton;
 import java.util.List;
 import java.util.ArrayList;
+import java.util.concurrent.CompletableFuture;
 
 import static play.libs.Scala.asScala;
 import lyc.*;
-/**
- * An example of form processing.
- *
- * https://playframework.com/documentation/latest/JavaForms
- */
+
+
 @Singleton
 public class WidgetController extends Controller {
-    private Connection twitter;
+    private final CompletableFuture<Connection> twitter;
+    private CompletableFuture<List<Item>> tweets;
     private final Form<WidgetData> form;
-    private List<Item> tweets;
 
     @Inject
     public WidgetController(FormFactory formFactory) {
         this.form = formFactory.form(WidgetData.class);
-        this.tweets = new ArrayList<Item>();
+        this.tweets = CompletableFuture.supplyAsync(() -> new ArrayList<Item>());  // initialize
 
         String []auths = new String[]{
                 "WUZeAUiJyJFySY2I5C7oTkaRB",
@@ -36,21 +34,21 @@ public class WidgetController extends Controller {
                 "mue5UQ1QWSwGWDgf1lDTnrSeLJFVJLZltQxdyL34u0C0a"
         };
         AccountFactory factory = new TwitterAccountFactory();
-        twitter = factory.createAccount(auths);
+        this.twitter = CompletableFuture.supplyAsync( () -> factory.createAccount(auths) );
     }
 
     public Result index() {
-        return ok(views.html.index.render(form, asScala(tweets)));
+        return ok(views.html.index.render(form, asScala(tweets.join())));
     }
 
 
     public Result search() {
-
-
         final Form<WidgetData> boundForm = form.bindFromRequest();
         WidgetData data = boundForm.get();
 
-        tweets = twitter.SearchPost(data.getKeyword(), 10);
+        tweets = twitter.thenApply(
+                (Connection conn) -> conn.SearchPost(data.getKeyword(), 10)
+        );
 
         //Logger message
         //play.Logger.ALogger logger = play.Logger.of(getClass());
@@ -63,11 +61,19 @@ public class WidgetController extends Controller {
 
     public Result userProfile(long user_id){
         UserBase user = TwitUserFactory.getInstance().getUserById(user_id);
-        List<Item> results = twitter.getHomeLineById(user_id);
+
+        CompletableFuture<List<Item>> results = twitter.thenApply(
+                (Connection conn) -> conn.getHomeLineById(user_id)
+        );
 
         if(user!=null)
-            return ok(views.html.userProfile.render(user, asScala(results)));
+            return ok(views.html.userProfile.render(user, asScala(results.join())));
         else
             return ok(views.html.error.render("User profile not found"));
+    }
+
+    public CompletableFuture<Result> testReturn(){
+        return CompletableFuture.supplyAsync( () ->
+             ok(views.html.error.render("User profile not found")) );
     }
 }
